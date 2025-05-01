c2033a4aa7b53a1d70d73a499ff43b31f985e7b5
Victor Aires <victorairesjp@gmail.com>
2025-05-01 20:30:57 -0300

    feat add List, metodo while

--- Projeto-Compras/src/edu/alura/main/App.java
+++ Projeto-Compras/src/edu/alura/main/App.java
@@ -1,5 +1,7 @@
 package edu.alura.main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import edu.alura.modelos.Cartao;
@@ -9,22 +11,36 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Produto> listaCompras = new ArrayList<>();
+
         Cartao cartao1 = new Cartao();
         System.out.println("Digite o limite do seu cartão: ");
         cartao1.setValorLimite(scanner.nextDouble());
         System.out.println("Cadastro feito com sucesso. Seu novo limite é de: R$ " + cartao1.getValorLimite());
 
-        Produto produto1 = new Produto();
-        System.out.println("Digite aqui o produto comprado: ");
-        produto1.setNome(scanner.next());
-        System.out.println("Digite aqui o valor gasto: ");
-        produto1.setPreco(scanner.nextDouble());
+        double totalGasto = 0;
+        double saldo = cartao1.getValorLimite();
 
-        System.out.println("Compra realizada: " + produto1.getNome());
-        System.out.println("Valor gasto: R$ " + produto1.getPreco());
+        while (totalGasto <= cartao1.getValorLimite()) {
+            if (totalGasto <= saldo) {
+                System.out.println("Digite aqui o produto comprado: ");
+                String nomeProduto = scanner.next();
+                System.out.println("Digite aqui o valor gasto: ");
+                double precoProduto = scanner.nextDouble();
+                Produto produto = new Produto(nomeProduto, precoProduto);
+        
+                listaCompras.add(new Produto(produto.getNome(), produto.getPreco()));
+                totalGasto += produto.getPreco();
+                saldo = cartao1.calcularSaldo(totalGasto);
+                System.out.println("Seu novo saldo é de R$ " + saldo);
+            }
+            else {
+                System.out.println("Você não pode mais gastar!");
+            }
+        }
 
-        double novoSaldo = cartao1.calcularSaldo(produto1.getPreco());
-        System.out.println("Seu novo saldo é de R$ " + novoSaldo);
+        System.out.println("Compras realizadas: " + listaCompras.toString());
+        System.out.println("Valor gasto: R$ " + totalGasto);
 
         scanner.close();
     }
--- Projeto-Compras/src/edu/alura/modelos/Produto.java
+++ Projeto-Compras/src/edu/alura/modelos/Produto.java
@@ -1,9 +1,16 @@
 package edu.alura.modelos;
 
+import java.util.List;
+
 public class Produto {
     private String nome;
     private double preco;
+    
 
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
     // Getters && Setters
     public String getNome() {
         return nome;
@@ -17,4 +24,18 @@ public class Produto {
     public void setPreco(double preco) {
         this.preco = preco;
     }    
+
+    // Methods
+    public double somarPrecos(List<Produto> listaCompras) {
+        double total = 0;
+        for (Produto item : listaCompras) {
+            total += item.getPreco();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: " + this.nome + " - Valor: R$ " + this.preco;
+    }
 }
